daef896c8665d35c82e36f90583e6b14f80925c4
LGRN424 <LGRN424@GMAIL.COM>
2016-10-20 15:05:16 -0400

    Range List Rework

---
+++ range_list-rework.py
@@ -0,0 +1,17 @@
+print "Ascending Order"
+print
+
+my_list = ['0', '1', '2', '3','4','5','6', '7', '8']
+my_list_len = len(my_list)
+for i in range(0,3,1):
+    print (my_list[i])
+print
+print "Descending Order"
+for i in range(3,-1,-1):
+    print (my_list[i])
+print
+print "Even Numbers and Reverse"
+for i in range(8,0,-2):
+    print (my_list[i])
+
+
