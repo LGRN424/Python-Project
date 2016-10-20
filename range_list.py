8dd958eef0c5478d5dc3de09d42bd52bf042a3c1
LGRN424 <LGRN424@GMAIL.COM>
2016-10-20 10:28:15 -0400

    Range Drills

---
+++ range_list.py
@@ -0,0 +1,24 @@
+print "Drill 1"
+print
+
+my_list = ['0', '1', '2', '3']
+my_list_len = len(my_list)
+for i in range(0,my_list_len):
+    print (my_list[i])
+print
+print "Drill 2"
+print
+
+my_list2 = ['0', '1', '2', '3', '4','5','6', '7', '8']
+
+my_list_len = len(my_list2)
+for i in range(3,-1,-1):
+    print (my_list2[i])
+print
+print "Drill 3"
+print
+my_list3 = ['0', '1', '2', '3', '4','5','6', '7', '8']
+
+my_list_len = len(my_list3)
+for i in range(8,0,-2):
+    print (my_list3[i])
